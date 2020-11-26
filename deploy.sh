4fdb5952fc5dc06f79bb894e8667bd07ce646c12
Minh Nhut Huynh <Ne7ven@gmail.com>
2020-11-27 10:52:14 +1100

    add custom domain from env variable

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo 'Copying index.html as 404.html'
 cp public/index.html public/404.html
 
 echo 'Deploying...'
-node_modules/.bin/surge --project ./public --domain cra-starter.surge.sh --token SURGE_TOKEN
+node_modules/.bin/surge --project ./public --domain SURGE_DOMAIN --token SURGE_TOKEN
 
 echo 'Deployed 🚀'
